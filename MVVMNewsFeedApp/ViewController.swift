79f134ce24eafa61078a04736fbbe9459a49c63b
Vishal <vishal.n@intelegain.com>
2020-05-19 14:52:52 +0530

    Initial Commit

---
+++ MVVMNewsFeedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMNewsFeedApp
+//
+//  Created by Vinayak Bhor on 19/05/20.
+//  Copyright © 2020 Vinayak Bhor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
