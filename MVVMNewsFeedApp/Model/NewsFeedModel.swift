bdeccfb326060d8e34eda733ea5c3a872ec9e6e8
Vishal <vishal.n@intelegain.com>
2020-07-23 12:51:13 +0530

    Added bug fixes.

--- MVVMNewsFeedApp/Model/NewsFeedModel.swift
+++ MVVMNewsFeedApp/Model/NewsFeedModel.swift
@@ -5,5 +5,40 @@
 //  Created by Vinayak Bhor on 19/05/20.
 //  Copyright © 2020 Vinayak Bhor. All rights reserved.
 //
-
+//Step 1:
 import Foundation
+
+
+struct DataStruct
+{
+    struct NewsList: Decodable
+    {
+       let hits:[HitsData]
+    }
+    struct HitsData:Decodable{
+        let title:String?
+        let author:String?
+        let story_title:String?
+    }
+
+    var poster_path: String
+
+    struct CodingKeys: CodingKey {
+        var intValue: Int?
+        var stringValue: String
+
+        init?(intValue: Int) { self.intValue = intValue; self.stringValue = "\(intValue)" }
+        init?(stringValue: String) { self.stringValue = stringValue }
+
+        static let poster_path = CodingKeys(stringValue: "hits")!
+        static func makeKey(poster_path: String) -> CodingKeys {
+            return CodingKeys(stringValue: poster_path)!
+        }
+    }
+
+    init(from coder: Decoder) throws {
+        let container = try coder.container(keyedBy: CodingKeys.self)
+        self.poster_path = try container.decode(String.self, forKey: .poster_path)
+        
+    }
+}
--- MVVMNewsFeedApp/View Controller/NewsFeedViewController.swift
+++ MVVMNewsFeedApp/View Controller/NewsFeedViewController.swift
@@ -7,24 +7,57 @@
 //
 
 import UIKit
-
+//step 4
 class NewsFeedViewController: UIViewController {
-
+    
+    let newsFeedViewModel:NewsFeedViewModel = NewsFeedViewModel()
+    var dataArray:[DataStruct.HitsData] = []
+    var indexValue:Int = 0
+    var pageNumber: Int = 0
+    var isFlag = true
+    
+    @IBOutlet weak var newsFeedTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        newsFeedViewModel.delegate = self
+        pageNumber = pageNumber+1
+        newsFeedTableView.delegate = self
+        newsFeedTableView.dataSource = self
+        newsFeedTableView.contentInset = UIEdgeInsets(top: 20.0, left: 0.0, bottom: 0.0, right: 0.0)
+        newsFeedTableView.rowHeight = UITableView.automaticDimension
+        newsFeedTableView.estimatedRowHeight = 44
+        fetchMovie()
         // Do any additional setup after loading the view.
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+           self.navigationItem.title = "News"
+       }
 
-    /*
-    // MARK: - Navigation
+    override func viewWillDisappear(_ animated: Bool) {
+           self.navigationItem.title = ""
+       }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func fetchMovie(){
+        newsFeedViewModel.fetchNewsFeedAPI(pageNo:1)
     }
-    */
 
 }
+extension NewsFeedViewController: NewsFeedDelegate {
+    func reloadFeedData(success: Bool, feedModel: DataStruct.NewsList) {
+         if !success {
+                   
+               }else{
+                   // reload list table
+                   dataArray = feedModel.hits
+                    newsFeedTableView.reloadData()
+                   
+               }
+    }
+    
+    
+
+}
+
